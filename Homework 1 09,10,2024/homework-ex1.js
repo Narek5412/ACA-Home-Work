df68610a5bae951b168c3401adacdf700eda4056
Narek5412 <narek.harutyunyan.2001.09@gmail.com>
2024-10-10 13:22:08 +0400

    new homework

---
+++ Homework 1 09,10,2024/homework-ex1.js
@@ -0,0 +1,9 @@
+//Given a number n ( n &gt; 0 ). Print Fibonacci series up to n.
+
+function number(num){
+let arr = [0,1];
+for(let i=0;arr[i]+arr[i+1]<num;i++){
+arr.push(arr[i]+arr[i+1]);
+}return arr.join(",")
+} console.log(number(7))
+console.log(number(45))
