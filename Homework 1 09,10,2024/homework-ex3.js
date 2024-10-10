f9cc8262d3e37b7207a835154256462e8ec82c2e
Narek5412 <narek.harutyunyan.2001.09@gmail.com>
2024-10-10 13:44:40 +0400

    new homework

---
+++ Homework 1 09,10,2024/homework-ex3.js
@@ -0,0 +1,14 @@
+//Given an array of numbers. Find the index of the second maximum element.
+function secondHighest(arr){
+    let highest = 0;
+    let highest2 = 0;
+for(let i=0;i<arr.length;i++){
+    if(arr[i]>highest){
+        highest2 = highest;
+        highest = arr[i]
+    }else if(arr[i]<highest && arr[i]>highest2){
+        highest2 = arr[i]
+    }
+} return arr.indexOf(highest2)
+}console.log(number([23, -98, 0, -456, 12, 8]));
+console.log(number([-60, 2, 43, -18, 5, -19, 36, 7, 56 ]));
