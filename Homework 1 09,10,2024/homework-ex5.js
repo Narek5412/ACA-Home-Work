89a3f9ee365700ba3041bf379089ade862ba8baa
Narek5412 <narek.harutyunyan.2001.09@gmail.com>
2024-10-10 16:11:21 +0400

    new homework

---
+++ Homework 1 09,10,2024/homework-ex5.js
@@ -0,0 +1,18 @@
+//Given a sentence with missing words and an array of words. Replace all ‘_’ in a
+//sentence with the words from the array.
+function missingWords(str,arr){
+    let strArr = str.split("");
+    let j = 0
+    for(let i=0;i  < strArr.length;i++){
+       if(strArr[i] === "_"){
+           strArr[i]=arr[j]
+           j++
+       }
+    }return strArr.join("")
+
+}console.log(missingWords("_, we have a _.",
+["Houston", "problem"]))
+console.log(missingWords("If at _ you don’t _, try, try _.",
+["first", "succeed", "again"]))
+console.log(missingWords("May the _ _ _ _.",
+["Force", "be", "with", "you"]))
