bdc30da62d2452f0e9212d958c4d95ba249c1515
Narek5412 <narek.harutyunyan.2001.09@gmail.com>
2024-10-17 15:45:49 +0400

    change

--- homework 1/index.js
+++ homework 1/index.js
@@ -1,16 +1,16 @@
-//function string(number){
-//    let str ="";
-//    let num = number
-//    for(let i = 1;i<=number;i = i*10){
-//       if((num % 10) % 2 === 0 && str[0] % 2=== 0){
-//           str = (num % 10 + "-") + str;
-//           num = (num - (num % 10))/10
-//       }else{
-//           str = (num % 10) + str;
-//           num = (num - (num % 10))/10
-//       }
-//    }return str;
-//}console.log(string(2614411))
+function transformNumberToString(number){
+    let str ="";
+    let num = number
+    for(let i = 1;i<=number;i = i*10){
+       if((num % 10) % 2 === 0 && str[0] % 2=== 0){
+           str = (num % 10 + "-") + str;
+           num = (num - (num % 10))/10
+       }else{
+           str = (num % 10) + str;
+           num = (num - (num % 10))/10
+       }
+    }return str;
+}console.log(transformNumberToString(2614411))
 
 function string(num) {
     let str = "";
