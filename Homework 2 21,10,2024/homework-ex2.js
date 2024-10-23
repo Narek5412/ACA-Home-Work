ae8e0683f23ca8c9c8631e9694caee04fb46ab92
Narek5412 <narek.harutyunyan.2001.09@gmail.com>
2024-10-23 23:03:41 +0400

    change

--- Homework 2 21,10,2024/homework-ex2.js
+++ Homework 2 21,10,2024/homework-ex2.js
@@ -1,14 +1,13 @@
 function calculatorOfDivide(n){
-    //if we want to solve a problem with string
-    let sumOfDivide="1";
-    for(let i=3;i<=n;i= i + 2){
-        sumOfDivide += ` - 1/${i} ` ;
-    }return sumOfDivide;
+    let sumOfDivide=1;
+    let num = 1
+    for(let i=3;i<=n;i= i + 2) {
+        if (num % 2 === 0) {
+            sumOfDivide += 1 / i
+            num++
+        } else {
+            sumOfDivide -= 1 / i
+            num++
+        }
+    }return  sumOfDivide
 }console.log(calculatorOfDivide(9));
-function calculatorOfDivide1(n){
-    //if we want to solve a problem with number
-    let sumOfDivide1=1;
-    for(let i=3;i<=n;i= i + 2){
-        sumOfDivide1 -= 1/i ;
-    }return sumOfDivide1;
-}console.log(calculatorOfDivide1(8));
