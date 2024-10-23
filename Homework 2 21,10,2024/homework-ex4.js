e853b4fc760c375ba379d7f4aae94b05bf595868
Narek5412 <narek.harutyunyan.2001.09@gmail.com>
2024-10-23 18:17:08 +0400

    change

--- Homework 2 21,10,2024/homework-ex4.js
+++ Homework 2 21,10,2024/homework-ex4.js
@@ -1,12 +1,15 @@
 function squarePrint(height,row){
     let arr =[]
+
 for(let i=0;i < height; i++){
     arr.push([]);
-    for(let j = 0;j < row; j++){
-        if (i === 0 || i === height-1 || j === 0 || j === row-1 ){
-            arr[i].push("*")
+    let str = ""
+    arr[i].push(str);
+   while(arr[i][0].length < row * 3){
+        if (i === 0 || i === height-1 || arr[i][0].length === 0 || arr[i][0].length === (row -1) * 3 ){
+            arr[i][0] +=" * "
         }else {
-            arr[i].push(" ")
+            arr[i][0] +="   "
         }
     }
 }return arr
