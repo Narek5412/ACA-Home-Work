b83ab036793f57064ddb60eb5989599e3608e104
Narek5412 <narek.harutyunyan.2001.09@gmail.com>
2024-10-26 20:14:47 +0400

    change

--- Homework 2 21,10,2024/homework-ex2.js
+++ Homework 2 21,10,2024/homework-ex2.js
@@ -1,13 +1,13 @@
 function calculatorOfDivide(n){
     let sumOfDivide=1;
-    let num = 1
+    let num = true
     for(let i=3;i<=n;i= i + 2) {
-        if (num % 2 === 0) {
+        if (num === true) {
             sumOfDivide += 1 / i
-            num++
+            num = false
         } else {
             sumOfDivide -= 1 / i
-            num++
+            num = true
         }
     }return  sumOfDivide
 }console.log(calculatorOfDivide(9));
